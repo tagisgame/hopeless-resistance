cbdd68f66cefa28aa4a861fa2dfe668f1aab4f92
tagisgame <tagisgame@gmail.com>
2022-11-22 08:50:51 +0100

    Upgraded the track generation module

--- src/scripts/2dMain.gd
+++ src/scripts/2dMain.gd
@@ -3,6 +3,7 @@ extends Node2D
 # game setup variables
 export var starting_gamespeed : float # gamespeed at the beginning of the game
 export var gamespeed_increment : float # how faster should game gets each frame/block (tbd...)
+export var gamespeed_cap : float # maximum speed of the game
 
 
 var track_scene
@@ -13,5 +14,5 @@ func _ready():
 
 func _on_Track_ready():
 	track_scene = get_node("Track")
-	track_scene.setup(starting_gamespeed, gamespeed_increment)
+	track_scene.setup(starting_gamespeed, gamespeed_increment, gamespeed_cap)
 	
--- src/scripts/TrackBlock.gd
+++ src/scripts/TrackBlock.gd
@@ -1,14 +1,37 @@
-extends Sprite
+extends KinematicBody2D
 
-
-func setup(var blocks_type_data):
-	print (blocks_type_data)
-	pass
+var block_data # Dictionary with all data for each block from JSON
+var width # chosen width of the block
 
 func _ready():
 	pass 
 
+# setup the block
+func set_block_data(b_data):
+	block_data = b_data
+	var image_texture = ImageTexture.new()
+	var image = Image.new()
+	var image_path = "res://src/assets/track-blocks/img/" + b_data.get("ids")[randi() % b_data.get("ids").size()] + ".png"
+	image.load(image_path)
+	image_texture.create_from_image(image)
+	
+	get_node("Sprite").texture = image_texture
+	
+	width = b_data.get("width")[randi() % b_data.get("width").size()]
+	get_node("Sprite").region_rect.size = Vector2(width, 592)
+	
+	if block_data.get("obstacle_type") != "none":
+		var hitbox_shape = RectangleShape2D.new()
+		var hitbox_data = block_data.get("obstacle_hitboxes").get("hitbox")
+		var hitbox_pos1 = Vector2(hitbox_data.get("x1"), hitbox_data.get("y1"))
+		var hitbox_pos2 = Vector2(hitbox_data.get("x2"), hitbox_data.get("y2"))
+		var hitbox_size = hitbox_pos1 - hitbox_pos2
+		var hitbox_pos_center = hitbox_pos1 + 0.5 * hitbox_size
+		var hitbox_extents = hitbox_size * 0.5
+		
+		hitbox_shape.extents = hitbox_extents
+		
+		var hitbox_node = get_node("Obstacle_hitbox")
+		hitbox_node.position = hitbox_pos_center
+		hitbox_node.shape = hitbox_shape
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
